af8a454c65f4ce5e59403cd3f7a0387b7e1c04fe
Bram Schabbink <73956679+brambrulant@users.noreply.github.com>
2024-03-26 11:25:10 +0100

    move endpoint logic to webhook itself

--- src/app/api/stripe/events/route.ts
+++ src/app/api/stripe/events/route.ts
@@ -1,30 +1,53 @@
 import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import { Events, Tickets, Users } from '@db/tables.ts';
+import { eq } from 'drizzle-orm';
+import { emailTicketsToUser } from '@functions/sendEmail.tsx';
+import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/vercel-postgres';
 
-const makeTicketCallToDatabase = async (
-  eventId: string,
-  amount: string,
-  userId: string,
-  email: string
-) => {
-  try {
-    await fetch(
-      `https://www.sjoef.app/api/ticket` +
-        new URLSearchParams({
-          eventId: eventId,
-          amount: amount,
-          userId: userId,
-          email: email,
-        }),
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-  } catch (error) {
-    console.log('error', error);
-  }
+const getUserName = async (userId: string) => {
+  const db = drizzle(sql);
+  const user = await db.select().from(Users).where(eq(Users.id, userId));
+  return user[0]?.name;
+};
+
+const createTickets = async (eventId: number, amount: number, userId: string, email: string) => {
+  const db = drizzle(sql);
+  const secret = process.env.JWT_SECRET as unknown as jwt.Secret;
+
+  const tickets = Array.from({ length: amount }, () => {
+    const payload = {
+      event_id: eventId,
+      user_id: userId,
+    };
+
+    const token = jwt.sign(payload, secret);
+
+    return {
+      event_id: eventId,
+      user_id: userId,
+      jwt: token,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+  });
+
+  await db.insert(Tickets).values(tickets).execute();
+
+  const event = await db.select().from(Events).where(eq(Events.id, eventId));
+
+  // update the tickets_sold count
+  await db
+    .update(Events)
+    .set({ tickets_sold: event[0]?.tickets_sold ? event[0]?.tickets_sold + amount : amount })
+    .where(eq(Events.id, eventId));
+
+  console.log('updated event');
+
+  const userName = await getUserName(userId);
+
+  await emailTicketsToUser(tickets, userName!, email!);
 };
 
 export const POST = async (req: NextRequest) => {
@@ -37,13 +60,13 @@ export const POST = async (req: NextRequest) => {
   switch (body.type) {
     case 'checkout.session.completed':
       console.log('metadata', body.data.object.metadata);
-      eventId = body.data.object.metadata.eventId;
-      amount = body.data.object.metadata.amount;
+      eventId = parseInt(body.data.object.metadata.eventId);
+      amount = parseInt(body.data.object.metadata.amount);
       email = body.data.object.metadata.email;
       userId = body.data.object.metadata.userId;
 
       console.log('event from stripe, checkout.session.completed', eventId, amount);
-      await makeTicketCallToDatabase(eventId, amount, userId, email);
+      await createTickets(eventId, amount, userId, email);
       break;
     default:
       console.log('event from stripe, unimportant');

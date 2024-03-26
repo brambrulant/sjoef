921b7fd514661d64b8d5c4ff33cdae6bc397106a
Bram Schabbink <73956679+brambrulant@users.noreply.github.com>
2024-03-26 07:45:49 +0100

    fix kinde auth import

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar.tsx';
-import { KindeUser } from '@kinde-oss/kinde-auth-react/dist/types/state/types';
+import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 
 export interface AvatarComponentProps {
   user: KindeUser;
